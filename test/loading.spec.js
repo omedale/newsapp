9f0925eb3fed4316ba12f795b0e84228d58c19db
oluwafemi medalem <oluwafemi.medale@andela.com>
2017-06-29 20:04:30 +0100

    chore(feedback-fix) feedback fixes

--- test/loading.spec.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { mount } from 'enzyme';
-import LoadingComponent from '../src/component/LoadingComponent';
-
-describe('Loading Component :', () => {
-  it('renders without crashing', () => {
-    mount(<LoadingComponent />);
-  });
-});
